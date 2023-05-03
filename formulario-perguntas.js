f354051036cf8da1bdd610d2dabae15b3f7e1a02
tiagoparreiral <tiagoparreiral@gmail.com>
2023-05-03 00:39:43 -0300

    adicionando projeto

---
+++ formulario-perguntas.js
@@ -0,0 +1,17 @@
+desafio dia 2
+
+let nome = prompt("Qual é o seu nome?");
+let idade = prompt("Quantos anos você tem?");
+let linguagem = prompt("Qual linguagem de programação você está estudando?");
+
+alert(`Olá ${nome}, você tem ${idade} anos e já está aprendendo ${linguagem}!`);
+
+let gostaDeLinguagem = prompt(`Você gosta de estudar ${linguagem}? Responda com o número 1 para SIM ou 2 para NÃO.`);
+
+if (gostaDeLinguagem === "1") {
+  alert("Muito bom! Continue estudando e você terá muito sucesso.");
+} else if (gostaDeLinguagem === "2") {
+  alert("Ahh que pena... Já tentou aprender outras linguagens?");
+} else {
+  alert("Resposta inválida. Por favor, responda com o número 1 para SIM ou 2 para NÃO.");
+}
