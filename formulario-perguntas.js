1a5f4dbfe9cb26c0614e27bf2cdb66c146984e60
tiagoparreiral <tiagoparreiral@gmail.com>
2023-05-15 11:55:33 -0300

    alteração pergunta 2

--- formulario-perguntas.js
+++ formulario-perguntas.js
@@ -1,7 +1,7 @@
 desafio dia 2
 
 let nome = prompt("Qual o seu nome?");
-let idade = prompt("Quantos anos você tem?");
+let idade = prompt("Qual a sua idade?");
 let linguagem = prompt("Qual linguagem de programação você está estudando?");
 
 alert(`Olá ${nome}, você tem ${idade} anos e já está aprendendo ${linguagem}!`);
