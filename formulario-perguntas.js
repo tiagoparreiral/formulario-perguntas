e325eb5a2650b5df3f37876e3a5cddeda0f0c09c
tiagoparreiral <tiagoparreiral@gmail.com>
2023-05-15 11:57:12 -0300

    congratulações

--- formulario-perguntas.js
+++ formulario-perguntas.js
@@ -9,7 +9,7 @@ alert(`Olá ${nome}, você tem ${idade} anos e já está aprendendo ${linguagem}
 let gostaDeLinguagem = prompt(`Você gosta de estudar ${linguagem}? Responda com o número 1 para SIM ou 2 para NÃO.`);
 
 if (gostaDeLinguagem === "1") {
-  alert("Muito bom! Continue estudando e você terá muito sucesso.");
+  alert("Parabéns! Continue estudando e você terá muito sucesso.");
 } else if (gostaDeLinguagem === "2") {
   alert("Ahh que pena... Já tentou aprender outras linguagens?");
 } else {
